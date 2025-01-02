e1010e3e67b2977baef68543ed6242aba4f30a89
vyshnavi-17 <112465832+vyshnavi-17@users.noreply.github.com>
2025-01-02 17:31:25 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 # Create a Flask app instance
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Define a route for the home page
 @app.route('/')
@@ -10,6 +10,6 @@ def home():
     return "Hello, Greatcoders team, How are you team"
 
 # Run the application
-if _name_ == '_main_':
+if __name__ == '__main__':
     port=int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
